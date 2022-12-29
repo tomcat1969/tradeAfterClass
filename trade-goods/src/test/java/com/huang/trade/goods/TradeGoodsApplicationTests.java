6433b92a91dab76af3198e2005c59d893b63055e
Lin Huang <tomcat1969@gmail.com>
2022-12-29 02:02:03 -0800

    initiate project and this is class 2 point

---
+++ trade-goods/src/test/java/com/huang/trade/goods/TradeGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.trade.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ trade-order/src/main/java/com/huang/trade/order/TradeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.huang.trade.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeOrderApplication.class, args);
+    }
+
+}
